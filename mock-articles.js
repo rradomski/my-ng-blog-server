2f8d64839344e03f4a1989f3783ab2444428ecee
Rafał Radomski <rafal.radomski00@gmail.com>
2019-10-12 02:45:56 +0100

    Mock articles, GET API and CORS package

---
+++ mock-articles.js
@@ -0,0 +1,24 @@
+module.exports = [
+    {
+        id: 1,
+        title: 'My first article',
+        content: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. In porta augue scelerisque neque finibus faucibus. Donec ut gravida quam. Duis condimentum metus libero, at congue ipsum vehicula in. Integer fringilla facilisis nisl ut tincidunt. Mauris sed vulputate tellus. Etiam ut tempor ipsum. Phasellus dignissim sem sed mi tristique feugiat.\n' +
+            '\n' +
+            'Maecenas rutrum justo non ipsum interdum, sit amet congue erat aliquam. Fusce eget neque fringilla, tempor nibh eu, ultrices quam. In id tincidunt lacus. Donec laoreet volutpat nisi, a aliquet mi dignissim pretium. Mauris ut aliquet nulla. Etiam venenatis dignissim volutpat. Nam a enim vulputate, aliquam nulla condimentum, consequat metus. Maecenas semper ornare nulla quis varius. Donec ac metus commodo, vulputate dui sed, consequat augue. Praesent egestas id arcu nec elementum. Nam ut dui vitae orci volutpat fringilla vitae nec ligula. Vestibulum consequat tellus eu ultrices rhoncus. Fusce ipsum turpis, luctus nec tincidunt ut, laoreet id velit. Quisque nec massa vestibulum, luctus metus at, feugiat diam. Proin lacinia lacus in enim venenatis posuere. Aliquam pellentesque est augue, at faucibus turpis lacinia eget. ',
+        description: 'This is my first mocked article. Don\'t read it. It\'s just mock. :P',
+        key: 'my-first-mock-article',
+        date: new Date(),
+        imageUrl: 'https://cdn-images-1.medium.com/max/184/1*nbJ41jD1-r2Oe6FsLjKaOg@2x.png'
+    },
+    {
+        id: 2,
+        title: 'Second article of mine',
+        content: ' Proin non pellentesque lacus, sed dapibus sem. Vestibulum imperdiet porttitor nisl eu vehicula. Praesent faucibus libero sit amet consequat interdum. Duis mauris risus, mattis a ligula sit amet, pharetra placerat ante. Aliquam mi augue, scelerisque ac sapien a, pharetra tincidunt enim. In malesuada eros quis tellus eleifend scelerisque. Sed vitae ornare metus. Ut sit amet mauris elit. Morbi vitae mauris vel augue pharetra fermentum. Vivamus auctor, lacus sit amet accumsan posuere, arcu nibh sodales nibh, id semper nulla erat quis lacus. Pellentesque vel arcu non tellus tincidunt viverra a vitae dolor.\n' +
+            '\n' +
+            'Pellentesque quis sodales risus. Donec pellentesque mauris turpis, sit amet luctus nulla sodales in. Morbi suscipit dictum dapibus. Nulla facilisi. Praesent lobortis lacus nisl, in pretium mauris ornare eu. Donec lacinia felis ac tincidunt semper. Aliquam sit amet blandit nisl. In sit amet eros consequat, pharetra mi id, mollis turpis. Nunc metus sem, facilisis eu augue et, condimentum ullamcorper ante. Aliquam non sodales enim, a placerat est. Sed ornare porta ex, nec pellentesque nulla rhoncus eu. Ut luctus magna lacinia, cursus mi sed, sagittis mauris. Integer venenatis, velit sit amet tristique egestas, purus enim posuere orci, et finibus odio mauris eu felis. Mauris a tellus fringilla, ullamcorper sapien porttitor, sagittis purus. Mauris consequat ornare mauris, sollicitudin ornare justo venenatis ut. Morbi fermentum, neque id euismod tincidunt, tortor mi fringilla tortor, et finibus ligula tellus eget enim. ',
+        description: 'Another mocked article. Read it. It\'s just mock. :P',
+        key: 'second-mock-article',
+        date: new Date(),
+        imageUrl: 'https://cdn-images-1.medium.com/max/184/1*nbJ41jD1-r2Oe6FsLjKaOg@2x.png'
+    }
+];
--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const express = require('express');
 
 const app = express();
 
-
 const PORT = 8000;
 
+const cors = require('cors');
+
+var corsOptions = {
+    origin: ['http://localhost:4000', 'http://localhost:4200']
+};
+
+app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/", function (req, res) {
+    res.send("hello from node :P");
+});
+
+require("./articles")(app);
